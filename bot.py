f3c4776d1dcc16d1d6ca15f5205739bfb97bf972
Omar Cardona <omar_cardon@hotmail.com>
2022-11-19 13:59:46 -0500

    Se completo el boot de GASTOS

--- bot.py
+++ bot.py
@@ -3,94 +3,213 @@ from config import bot
 import config
 from time import sleep
 import re
-from telebot import types
 import logic
+import database.db as db
+
+#########################################################
+if __name__ == '__main__':
+    db.Base.metadata.create_all(db.engine)
+
 #########################################################
 # Aquí vendrá la implementación de la lógica del bot  
 
 #Aplicacion Control de Gastos
 
-#Comando: /start
 @bot.message_handler(commands=['start'])
 def on_command_start(message):
     bot.send_chat_action(message.chat.id, 'typing')
-    sleep(1)
+
     bot.send_message(
         message.chat.id,
-        "Hola, soy un \U0001F916, ¿cómo *estás*? \U0001F61C",
+        logic.get_welcome_message(bot.get_me()),
         parse_mode="Markdown")
+    
+    bot.send_message(
+        message.chat.id,
+        logic.get_help_message(),
+        parse_mode="Markdown")
+    
+    logic.register_account(message.from_user.id)
 
-#Ayuda
+#########################################################
 @bot.message_handler(commands=['help'])
 def on_command_help(message):
     bot.send_chat_action(message.chat.id, 'typing')
-    sleep(1)
-    response = (
-        "Estos son los comandos y órdenes disponibles:\n"
-        "\n"
-        "*/start* - Inicia la interacción con el bot\n"
-        "*/help* - Muestra este mensaje de ayuda\n"
-        "*sumar {valor1} y {valor2}* - Calcula la suma de dos valores\n"
-        #"*restar {valor1} y {valor2}* - Calcula la resta de dos valores\n"
-        #"*multiplicar {valor1} y {valor2}* - Calcula la multiplicación de dos valores\n"
-        "*dividir {valor1} y {valor2}* - Calcula la división de dos valores\n"
-        )
+
     bot.send_message(
         message.chat.id,
-        response,
-        parse_mode="Markdown")    
-
-#Menu
-@bot.message_handler(commands=['menu'])
-def on_command_menu(message):
-    # Using the ReplyKeyboardMarkup class
-    # It's constructor can take the following optional arguments:
-    # - resize_keyboard: True/False (default False)
-    # - one_time_keyboard: True/False (default False)
-    # - selective: True/False (default False)
-    # - row_width: integer (default 3)
-    # row_width is used in combination with the add() function.
-    # It defines how many buttons are fit on each row before continuing on the next row.
-    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
-    itembtn1 = types.KeyboardButton('/start')
-    itembtn2 = types.KeyboardButton('/help')
+        logic.get_help_message(),
+        parse_mode="Markdown")
+#########################################################
+@bot.message_handler(commands=['about'])
+def on_command_about(message):
+    bot.send_chat_action(message.chat.id, 'typing')
     
-    markup.add(itembtn1, itembtn2)
+    bot.send_message(
+        message.chat.id,
+        logic.get_about_this(config.VERSION),
+        parse_mode="Markdown")
+#########################################################
+@bot.message_handler(regexp=r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_earn_money(message):
+    bot.send_chat_action(message.chat.id, 'typing')
 
-    bot.send_message(message.chat.id, "Selecciona una opción del menú:",
-        reply_markup=markup)      
+    parts = re.match(
+        r"^(gane|gané|g) ([+-]?([0-9]*[.])?[0-9]+)$",
+        message.text,
+        flags=re.IGNORECASE)
 
-
-
+    # print (parts.groups())
+    amount = float(parts[2])
+    control = logic.earn_money (message.from_user.id, amount)
+    bot.reply_to(
+        message,
+    f"\U0001F4B0 ¡Dinero ganado!: {amount}" if control == True
+    else "\U0001F4A9 Tuve problemas registrando la transacción, ejecuta /start y vuelve a intentarlo")
 
 #########################################################
-# Persistencia a Datos
-bot_data = {}
+@bot.message_handler(regexp=r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$")
+def on_spend_money(message):
+    bot.send_chat_action(message.chat.id, 'typing')
 
-class Record:
-    def __init__(self):
-        self.height = None
-        self.weight = None
-        self.gender = None   
+    parts = re.match(
+        r"^(gaste|gasté|gg) ([+-]?([0-9]*[.])?[0-9]+)$",
+        message.text,
+        flags=re.IGNORECASE)
 
-# Enable saving next step handlers to file "./.handlers-saves/step.save".
-# Delay=2 means that after any change in next step handlers (e.g. calling register_next_step_handler())
-# saving will hapen after delay 2 seconds.
-bot.enable_save_next_step_handlers(delay=2)
-# Load next_step_handlers from save file (default "./.handlers-saves/step.save")      
-# WARNING It will work only if enable_save_next_step_handlers was called!
-bot.load_next_step_handlers()  
+    amount = float(parts[2])
+    control = logic.spend_money (message.from_user.id, amount)
 
+    bot.reply_to(
+        message,
+        f"\U0001F4B8 ¡Dinero gastado!: {amount}" if control == True
+        else "\U0001F4A9 Tuve problemas registrando la transacción, ejecuta /start y vuelve a intentarlo")
 #########################################################
 
+@bot.message_handler(regexp=r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_earnings(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    parts = re.match(
+        r"^(listar ganancias|lg) en ([0-9]{1,2}) de ([0-9]{4})$",
+        message.text,
+        flags=re.IGNORECASE)
+
+    month = int(parts[2])
+    year = int(parts[3])
+    
+    if month < 1 or month > 12:
+        bot.reply_to(message, f"Error, mes inválido: {month}")
+        return
+
+    if year < 1990 or year > 2050:
+        bot.reply_to(message, f"Error, año inválido: {year}")
+        return
+
+    earnings = logic.list_earnings (message.from_user.id, month, year)
+
+    text = ""
+    total = 0
+    
+    if not earnings:
+        text = f"\U0001F633 No tienes ganancias registradas en {month}/{year}"
+    else:
+        text = "``` Listado de ganancias:\n\n"
+        for e in earnings:
+            total += e.amount
+            text += f"| {e.id} | ${e.amount} | ({e.when.strftime('%d/%m/%Y - %H:%M')}) |\n"
+        text += f"\nTOTAL = ${total}"
+        text += "```"
+    
+    bot.reply_to(message, text, parse_mode="Markdown")
+#########################################################
+@bot.message_handler(regexp=r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$")
+def on_list_spendings(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    
+    parts = re.match(
+        r"^(listar gastos|lgg) en ([0-9]{1,2}) de ([0-9]{4})$",
+        message.text,
+        flags=re.IGNORECASE)
+    
+    month = int(parts[2])
+    year = int(parts[3])
+    
+    if month < 1 or month > 12:
+        bot.reply_to(message, f"Error, mes inválido: {month}")
+        return
+    
+    if year < 1990 or year > 2050:
+        bot.reply_to(message, f"Error, año inválido: {year}")
+        return
+    
+    spendings = logic.list_spendings (message.from_user.id, month, year)
+    
+    text = ""
+    total = 0
+    
+    if not spendings:
+        text = f"\U0001F633 No tienes gastos registrados en {month}/{year}"
+    else:
+        text = "``` Listado de gastos:\n\n"
+        for s in spendings:
+            total += s.amount
+            text += f"| {s.id} | ${s.amount} | ({s.when.strftime('%d/%m/%Y - %H:%M')}) |\n"
+    
+        text += f"\nTOTAL = ${total}"
+        text += "```"
+    
+    bot.reply_to(message, text, parse_mode="Markdown")
+#########################################################
+@bot.message_handler(regexp=r"^(obtener saldo|s)$")
+def on_get_balance(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    
+    balance = logic.get_balance (message.from_user.id)
+    
+    text = "\U0000274C Aún no tienes una cuenta asociada, ejecuta /start para arreglarlo."
+    if balance != None:
+        text = f"Tu saldo actual es ${balance}"
+    bot.reply_to(message, text)
+#########################################################
+@bot.message_handler(regexp=r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$")
+def on_remove_record(message):
+    bot.send_chat_action(message.chat.id, 'typing')
+    
+    parts = re.match(
+        r"^(remover|r) (ganancia|g|gasto|gg) ([0-9]+)$",
+        message.text,
+        flags=re.IGNORECASE)
+    
+    record_type = parts[2]
+    index = int(parts[3])
+    
+    if record_type not in ["ganancia", "g", "gasto", "gg"]:
+        bot.reply_to(message, f"Error, tipo de registro inválido:{record_type}")
+        return
+
+    if index < 0:
+        bot.reply_to(message, f"Error, índice inválido: {index}")
+        return
+    
+    response = False
+    
+    if record_type == "ganancia" or record_type == "g":
+        response = logic.remove_earning (message.from_user.id, index)
+    elif record_type == "gasto" or record_type == "gg":
+        response = logic.remove_spending (message.from_user.id, index)
+    if response:
+        bot.reply_to(message, f"Registro removido: {record_type}, {index}")
+    else:
+        bot.reply_to(message, f"No se pudo remover el registro: {index}")
+#########################################################
 #Default
 @bot.message_handler(func=lambda message: True)
 def on_fallback(message):
     bot.send_chat_action(message.chat.id, 'typing')
     sleep(1)
-    bot.reply_to(
-        message,
-        "\U0001F63F Ups, no entendí lo que me dijiste.")        
+
+    response = logic.get_fallback_message(message.text)
+    bot.reply_to(message, response)       
 
 ######################################################### 
 #Hace el llamado a Telegram c/20 segundos para ver si hay mensajes
